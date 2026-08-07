7f984cfd52fd77597cbec958dd26ce957c9aab69
sharafadeen hammed <sharafadeenhammed@yahoo.com>
2026-08-07 15:21:09 +0100

    disabled reloader

--- main.py
+++ main.py
@@ -156,4 +156,4 @@ async def predict(file: UploadFile = File(...)):
     
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
